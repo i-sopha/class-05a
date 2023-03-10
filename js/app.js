30afdf15386125df42f1066cbc539737c52df033
Isabel Sopha <i.sopha94@gmail.com>
2023-03-10 12:00:49 -0800

    test for multiply passed

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+// function sum(a, b) { //eslint-disable-line
 
-}
+// }
 
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
@@ -27,11 +27,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    return [a * b, 'The product of 5 and 9 is 45.'];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
